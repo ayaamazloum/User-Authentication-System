d84b8c909dcea2f0542569c3582fbf9250ef4759
ayaamazloum <ayamazloum.a@gmail.com>
2024-07-29 12:25:01 +0300

    Frontend: Create auth service

---
+++ auth_app/lib/src/services/auth_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  final String apiUrl = 'http://192.168.0.115:8000/api';
+  final storage = FlutterSecureStorage();
+
+  Future<void> login(String email, String password) async {
+    print('response.statusCode');
+    final response = await http.post(
+      Uri.parse('$apiUrl/login'),
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      await storage.write(key: 'token', value: data['token']);
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+
+  Future<void> logout() async {
+    final token = await storage.read(key: 'token');
+    final response = await http.post(
+      Uri.parse('$apiUrl/logout'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      await storage.delete(key: 'token');
+    } else {
+      throw Exception('Failed to logout');
+    }
+  }
+}
