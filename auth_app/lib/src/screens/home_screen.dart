b8e6c7b25e8d4a22aa945f889975d87bc5030c98
ayaamazloum <ayamazloum.a@gmail.com>
2024-07-29 14:30:17 +0300

    Frontend: Add login functionality to home screen

--- auth_app/lib/src/screens/home_screen.dart
+++ auth_app/lib/src/screens/home_screen.dart
@@ -1,13 +1,37 @@
+import 'package:auth_app/src/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  final _authService = AuthService();
+
+  void logout(BuildContext context) async {
+    try {
+      await _authService.logout();
+      Navigator.of(context).pushReplacementNamed('/login');
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Welcome to the Home Screen!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to the Home Screen!'),
+            ElevatedButton(
+              onPressed: () {
+                logout(context);
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
