b2f2a4dc21f2d6c592eb1437702afe56890b4f39
ayaamazloum <ayamazloum.a@gmail.com>
2024-07-29 11:58:53 +0300

    Backend: Create user authentication apis

--- auth_backend/app/Http/Controllers/AuthController.php
+++ auth_backend/app/Http/Controllers/AuthController.php
@@ -32,4 +32,31 @@ class AuthController extends Controller
 
         return response()->json(compact('user', 'token'));
     }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return response()->json(['token' => auth()->refresh()]);
+    }
 }
